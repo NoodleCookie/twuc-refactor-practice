62ab139ca60cafe94fc68b8ca1983c8f5330c914
Izumi <1042631369@qq.com>
2021-08-12 22:03:12 +0800

    [Izumi] ref: 重构equals方法

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -41,17 +41,12 @@ public class Direction {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
-        Direction direction1 = (Direction) o;
-
-        if (direction != direction1.direction) return false;
-
-        return true;
+        return direction == ((Direction) o).direction;
     }
 
     @Override
     public int hashCode() {
-        return (int) direction;
+        return direction;
     }
 
     @Override
